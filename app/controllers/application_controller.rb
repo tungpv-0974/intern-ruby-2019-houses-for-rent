443dac8b1357370be0e0f496e0bb462ba90958bf
tungpv-0974 <phan.van.tung@sun-asterisk.com>
2020-01-14 15:03:22 +0700

    add rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ?
-      locale : I18n.default_locale
+    if I18n.available_locales.include? locale
+      I18n.locale = locale
+    else
+      I18n.default_locale
+    end
   end
 end
