5be5e8671cd85049e54fa77171e77b9ffb934d14
tungpv-0974 <phan.van.tung@sun-asterisk.com>
2020-02-06 09:38:27 +0700

    Create Post

--- app/models/post_picture.rb
+++ app/models/post_picture.rb
@@ -1,3 +1,4 @@
 class PostPicture < ApplicationRecord
   belongs_to :post
+  mount_uploader :image_url, PictureUploader
 end
--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,7 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 require 'carrierwave/orm/activerecord'
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
